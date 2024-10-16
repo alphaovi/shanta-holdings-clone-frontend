d8a3124038ead568a9d29257264ca6c0af3638cc
alphaovi <ovi778811@gmail.com>
2024-10-16 18:34:11 +0600

    vissin and mission updated

--- src/Pages/About/OurStory/VissionAndMission.jsx
+++ src/Pages/About/OurStory/VissionAndMission.jsx
@@ -4,9 +4,16 @@ import visionImage from "../../../assets/images/About/Our-story/vision-image.png
 const VissionAndMission = () => {
   return (
     <section className=" bg-[#5C5151] text-white">
-      <div className="flex mx-20 p-20">
+      <div className="lg:flex mx-20 p-20">
         <div className="">
-          <h1 className="text-6xl "><span className="mr-10">V</span><span className="mr-10">I</span><span className="mr-10">S</span><span className="mr-10">I</span><span className="mr-10">O</span><span>N</span></h1>
+          <h1 className="text-6xl ">
+            <span className="mr-10">V</span>
+            <span className="mr-10">I</span>
+            <span className="mr-10">S</span>
+            <span className="mr-10">I</span>
+            <span className="mr-10">O</span>
+            <span>N</span>
+          </h1>
           <p className="text-xl mr-10 my-10">
             To become the most trusted brand in the region by offering
             comprehensive solutions to the diverse needs of the communities we
@@ -14,13 +21,21 @@ const VissionAndMission = () => {
           </p>
           <img src={visionImage} alt="Vision Image" />
         </div>
-        <div className="scroll-m-8 hover:scroll-m-0">
-          <h1 className="text-6xl "><span className="mr-10">M</span><span className="mr-10">I</span><span className="mr-10">S</span><span className="mr-10">S</span><span className="mr-10">I</span><span className="mr-10">O</span><span>N</span></h1>
+        <div className="transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 duration-300">
+          <h1 className="text-6xl ">
+            <span className="mr-10">M</span>
+            <span className="mr-10">I</span>
+            <span className="mr-10">S</span>
+            <span className="mr-10">S</span>
+            <span className="mr-10">I</span>
+            <span className="mr-10">O</span>
+            <span>N</span>
+          </h1>
           <p className="text-xl mr-10 my-10">
             To set new standards through innovation and sustainable solutions
             that exceed the expectations of all our stakeholders.
           </p>
-          <img src={missionImage} alt="Mission Image" />
+          <img className="w-full" src={missionImage} alt="Mission Image" />
         </div>
       </div>
     </section>
